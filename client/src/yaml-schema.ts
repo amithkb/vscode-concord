da6015c82ceb3d7eff3b5d4c368909c53dcf5b13
a0k04u1 <amith.k.b@walmart.com>
2024-07-16 14:13:21 +0530

    init commit

--- client/src/yaml-schema.ts
+++ client/src/yaml-schema.ts
@@ -1,10 +1,11 @@
 import * as vscode from 'vscode';
 import * as semver from 'semver';
+
 const VSCODE_YAML_EXTENSION_ID = "redhat.vscode-yaml";
 
 declare type YamlSchemaContributor = (schema: string,
-    requestSchema: (resource: string) => string | undefined,
-    requestSchemaContent: (uri: string) => string) => void;
+                                      requestSchema: (resource: string) => string | undefined,
+                                      requestSchemaContent: (uri: string) => string) => void;
 
 
 export async function registerYamlSchemaSupport(): Promise<void> {
@@ -13,7 +14,8 @@ export async function registerYamlSchemaSupport(): Promise<void> {
         return;
     }
 }
-export async function activateYamlExtension(): Promise<{registerContributor: YamlSchemaContributor} | undefined> {
+
+export async function activateYamlExtension(): Promise<{ registerContributor: YamlSchemaContributor } | undefined> {
     const ext = vscode.extensions.getExtension(VSCODE_YAML_EXTENSION_ID);
     if (!ext) {
         vscode.window.showWarningMessage('Please install \'YAML Support by Red Hat\' via the Extensions pane.');
@@ -24,7 +26,7 @@ export async function activateYamlExtension(): Promise<{registerContributor: Yam
         vscode.window.showWarningMessage('The installed Red Hat YAML extension doesn\'t support Intellisense. Please upgrade \'YAML Support by Red Hat\' via the Extensions pane.');
         return undefined;
     }
-  
+
     if (ext.packageJSON.version && !semver.gte(ext.packageJSON.version, '0.0.15')) {
         vscode.window.showWarningMessage('The installed Red Hat YAML extension doesn\'t support multiple schemas. Please upgrade \'YAML Support by Red Hat\' via the Extensions pane.');
     }
